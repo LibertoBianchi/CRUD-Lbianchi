59e2d369687245f46da365d56fb7a8f8c2826c76
LibertoBianchi <lbianchi@cifpfbmoll.eu>
2022-02-07 22:54:09 +0100

    e

--- CRUD-Lbianchi/app/Http/Controllers/ArticuloController.php
+++ CRUD-Lbianchi/app/Http/Controllers/ArticuloController.php
@@ -123,9 +123,6 @@ class ArticuloController extends Controller
     public function destroy($id)
     {
 
-        if (!Gate::allows('Admin')) {
-            abort(403);
-        }
 
 
         $articulo = Articulo::find($id);
